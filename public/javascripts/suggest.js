00ead88e318e00dd3e59a39a6ced462af0fa433b
masahirompp <masahirompp@gmail.com>
2013-11-23 18:52:18 +0900

    suggesting...

--- public/javascripts/suggest.js
+++ public/javascripts/suggest.js
@@ -1,16 +1,19 @@
 function SuggestCtrl($scope) {
 
-  var baseUrl = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=perfume&country=JP&entity=musicTrack';
+  var baseUrl = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=@term&country=JP&entity=musicTrack';
 
-  $.ajax({
-    url: baseUrl,
-    dataType: 'jsonp',
-    success: function(data){
-      alert(data.results[0].trackName);
-      $scope.tracks = data.results;
-    },
-    error: function(data){
-      alert('error');
-    }
-  });
+  $scope.term = "";
+
+  $scope.suggest = function() {
+    $.ajax({
+      url: baseUrl.replace('@term',$scope.term),
+      dataType: 'jsonp',
+      success: function(data){
+        $scope.$apply(function(){
+          $scope.tracks = data.results;
+        });
+      }
+    });
+  };
+
 }

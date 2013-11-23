7e6c605c3a2ac65e774f988dd976cdf0f4094907
Lhacker <starclown1@gmail.com>
2013-11-23 19:30:43 +0900

    modify display of members.

--- app/views/chatRoom.scala.js
+++ app/views/chatRoom.scala.js
@@ -5,30 +5,35 @@ function chatRoomAppController($scope) {
     var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket;
     var chatSocket = new WS("@routes.Application.chat(username).webSocketURL()");
 
+    // init models
+    $scope.error = false;
+    $scope.messages = [];
+
+    // on receive message
     chatSocket.onmessage = function(e) {
         var data = JSON.parse(e.data);
 
-        // Handle errors
-        if(data.error) {
-            chatSocket.close();
-            $scope.error = data.error;
-            return;
-        } else {
-            $scope.error = '';
-        }
+        $scope.$apply(function() {
+            // Handle errors
+            if(data.error) {
+                chatSocket.close();
+                $scope.error = data.error;
+                return;
+            } else {
+                $scope.error = '';
+            }
 
-        // Create the message element
-        var el = $('<div class="message"><span></span><p></p></div>');
-        $("span", el).text(data.user);
-        $("p", el).text(data.message);
-        $(el).addClass(data.kind);
-        if(data.user == '@username') $(el).addClass('me');
-        $('#messages').append(el);
+            // Create the message element
+            $scope.messages.push({
+                user: data.user,
+                message: data.message,
+                kind: data.kind,
+                me: data.user === '@username'
+            });
 
-        // Update the members list
-        $scope.$apply = function() {
+            // Update the members list
             $scope.members = data.members;
-        };
+        });
     };
 
 }
